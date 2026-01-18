8c636d6cfdb65a1d48f1122c7ff54adf42af1e93
Muzaffer Barış Yavuz <csafter40@gmail.com>
2026-01-19 00:45:36 +0300

    Refactor entrypoint script to start application with provided command instead of fixed runserver command, enhancing flexibility for container execution.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -24,5 +24,5 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "Starting server..."
-exec python manage.py runserver 0.0.0.0:8000
+echo "Starting application..."
+exec "$@"
